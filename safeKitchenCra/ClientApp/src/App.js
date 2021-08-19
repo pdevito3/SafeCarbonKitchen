7ec7ec650dee68ae31efce516e65c139869b96db
Paul DeVito <pdevito3@gmail.com>
2021-08-18 23:52:24 -0400

    add RQ and axios

--- safeKitchenCra/ClientApp/src/App.js
+++ safeKitchenCra/ClientApp/src/App.js
@@ -4,6 +4,7 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
 import './custom.css'
 
@@ -12,11 +13,13 @@ export default class App extends Component {
 
   render () {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+      <QueryClientProvider client={new QueryClient()}>
+          <Layout>
+            <Route exact path='/' component={Home} />
+            <Route path='/counter' component={Counter} />
+            <Route path='/fetch-data' component={FetchData} />
+          </Layout>
+      </QueryClientProvider>
     );
   }
 }
---
+++ safeKitchenCra/ClientApp/src/apis/claims.jsx
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+const claimsKeys = {
+  claim: ['claims']
+  // claims: ['claims'],
+  // claims: (id) => [...claimsKeys.claims, id]
+}
+
+const config = {
+  headers: {
+    'X-CSRF': '1'
+  }
+}
+
+const fetchClaims = async () => 
+  axios.get('/bff/user', config)
+    .then((res) => res.data);
+
+
+function useClaims() {
+  return useQuery(
+    claimsKeys.claim, 
+    async () => fetchClaims(),
+    {
+    }
+  )  
+}
+
+export { useClaims as default }
